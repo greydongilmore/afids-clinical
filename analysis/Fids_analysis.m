2ffda66f1b66a6182100812ad7a3ec32d811f1ba
abbassm <mohamad.abbass26@gmail.com>
2019-08-05 11:25:48 -0400

    Stats

--- analysis/Fids_analysis.m
+++ analysis/Fids_analysis.m
@@ -2,8 +2,8 @@ clear
 clc
 fclose('all');
 
-data_dir = 'C:\Users\Greydon\Documents\github\afids_parkinsons\input\input_fid';
-% data_dir = 'D:\School\Residency\Research\FIDs Study\Github\afids_parkinsons\input\input_fid';
+% data_dir = 'C:\Users\Greydon\Documents\github\afids_parkinsons\input\input_fid';
+data_dir = 'D:\School\Residency\Research\FIDs Study\Github\afids_parkinsons\input\input_fid';
 
 sub_ignore = [];
 
@@ -30,7 +30,7 @@ raters = string(unique(Data.rater,'rows'));
 
 % Generates arrays for subjects completed by each rater
 Sub = {};
-size = [];
+Size_sub = [];
 for r = 1:length(raters)
     idx = ismember(Data.rater, raters(r));
     sub_temp = unique(Data.subject(idx,:), 'rows');
@@ -38,11 +38,11 @@ for r = 1:length(raters)
         sub_temp = sub_temp(~ismember(sub_temp, sub_ignore));
     end
     Sub{1,r} = sub_temp(~ismember(sub_temp, sub_ignore));
-    size(r) = length(Sub{1,r});
+    Size_sub(r) = length(Sub{1,r});
 end
 
 % Subjects completed by all raters
-[B,I] = sort(size, 'descend');
+[B,I] = sort(Size_sub, 'descend');
 Sub = Sub(I);
 Sub_Comp = intersect(Sub{1,1},Sub{1,2});
 for irate = 2:length(raters)
@@ -125,5 +125,57 @@ line([0,0], [0,0], 2*zl, 'LineWidth', 1, 'Color', 'k');
 xlabel('X coord')
 ylabel('Y coord')
 zlabel('Z coord')
+
+
+%% Inter-rater reliability
+
+% Using intraclass correlation (ICC(2,1)) as described by Shrout, P. E., & Fleiss, J. L. (1979). Intraclass correlations: Uses in assessing rater reliability. Psychological Bulletin, 86(2), 420-428.
+% Goal is calculate ICC for each fiducial for each axis
+
+% Matrix containing each statistic(BMS,JMS,WMS,EMS,ICC ; dim1) for each fid (dim 2) in each axis (dim 3)
+ICC_Stats = zeros(5,32,3);
+
+Raters_ICC = ["GG", "MA"];
+
+ICC_Data = squeeze(Tot_Data(:,2:4,:,ismember(raters,Raters_ICC)));
+
+% n = number of samples, k = number of raters
+[~,~,n,k] = size(ICC_Data);
+
+% Mean across subjects
+Mean_Sub = squeeze(mean(ICC_Data,3));
+
+% Mean across raters
+Mean_Rat = squeeze(mean(ICC_Data,4));
+
+% Grand mean
+Mean_Grand = squeeze(mean(Mean_Sub,3));
+
+% Calculate between-subject (target) mean square (BMS) for each fidicial +
+% axis
+ICC_Stats(1,:,:) = sum((((Mean_Rat - Mean_Grand).^2)*k),3)/(n-1);
+
+
+% Calculate between-rater (judge) mean square (JMS) for each fidicial +
+% axis
+
+ICC_Stats(2,:,:) = sum((((Mean_Sub - Mean_Grand).^2)*n),3)/(k-1);
+
+
+% Calculate within-target (subject) mean square (WMS) for each fidicial +
+% axis
+
+ICC_Stats(3,:,:) = squeeze(mean(var(ICC_Data,0,4),3));
+
+% Calculate within-target (subject) residual mean square (EMS) for each fidicial +
+% axis
+
+ICC_Stats(4,:,:) = (ICC_Stats(3,:,:)*(n*(k-1)) - ICC_Stats(2,:,:)*(k-1))/((n-1)*(k-1));
+
+% Calculate ICC
+
+ICC_Stats(5,:,:) = (ICC_Stats(1,:,:) - ICC_Stats(4,:,:))./(ICC_Stats(1,:,:) + ICC_Stats(4,:,:)*(k-1) + (ICC_Stats(3,:,:) - ICC_Stats(5,:,:))*k/n);
+
+Final_ICC = squeeze(ICC_Stats(5,:,:));
 
 
